fc2ca09a6bd3f54c2906f15a2e0f200ae63643bb
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-07-04 12:42:10 -0400

    add fs utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
+export * from './fs';
 export { default as Log, info, success, warn, error } from './logger';
 export { default as checkApp } from './check_app';
 export { default as checkMeteor } from './check_meteor';
