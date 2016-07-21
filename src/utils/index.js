520b38ac2a17692bb0fc9fa8e013fc0991f10349
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-07-21 08:04:03 -0700

    remove old logger exports

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
 export * from './fs';
-export { default as Log, info, success, warn, error } from './logger';
+export { default as Log } from './logger';
 export { default as checkApp } from './check_app';
 export { default as checkDeps } from './check_deps';
 export { default as checkMeteor } from './check_meteor';
