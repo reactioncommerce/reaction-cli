cf33ccc1e56461fce95c2c094a34ae8d2b410ad0
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-08-13 14:04:14 -0400

    fix callback in app checks

--- src/utils/check_deps.js
+++ src/utils/check_deps.js
@@ -1,11 +1,14 @@
 import includes from 'lodash/includes';
-import checkApp from './check_app';
+import checkIfInReactionDir from './check_app';
 import checkMeteor from './check_meteor';
-import Log from './logger';
+
 
 export default function (checks = [], callback) {
   if (includes(checks, 'app')) {
-    checkApp();
+    checkIfInReactionDir();
+    if (checks.length === 1) {
+      callback();
+    }
   }
   if (includes(checks, 'meteor')) {
     checkMeteor(callback);
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 export * from './fs';
 export { default as Log } from './logger';
-export { default as checkApp } from './check_app';
+export { default as checkIfInReactionDir } from './check_app';
 export { default as checkDeps } from './check_deps';
 export { default as checkMeteor } from './check_meteor';
 export { default as loadPlugins } from './plugin-loader';
