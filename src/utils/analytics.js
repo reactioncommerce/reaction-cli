c752cd9d04c0cf677795f5947009a838531aba88
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-08-14 11:45:55 -0400

    handle missing segment key

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -5,16 +5,20 @@ import { getUserId } from './config';
 import getVersions from './versions';
 
 const segmentKey = require('../../config.json').segment;
-const analytics = new Analytics(segmentKey, { flushAt: 1, flushAfter: 1 });
 
 const cmd = yargs.argv.$0;
 const args = process.argv.splice(2, process.argv.length).join(' ');
 
 export function track(cb) {
-  const properties = {
-    command: `${cmd} ${args}`,
-    argv: _.forEach(yargs.argv, (val) => !!val),
-    versions: getVersions()
-  };
-  analytics.track({ event: 'command', userId: getUserId(), properties }, cb);
+  if (!!segmentKey) {
+    const analytics = new Analytics(segmentKey, { flushAt: 1, flushAfter: 1 });
+    const properties = {
+      command: `${cmd} ${args}`,
+      argv: _.forEach(yargs.argv, (val) => !!val),
+      versions: getVersions()
+    };
+    analytics.track({ event: 'command', userId: getUserId(), properties }, cb);
+  } else {
+    cb();
+  }
 }
