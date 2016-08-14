7dd8c1221ef41e9252224281432d63d8b7ede9fa
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-08-14 13:27:46 -0400

    add os detection

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import os from 'os';
 import Analytics from 'analytics-node';
 import yargs from 'yargs';
 import { getUserId } from './config';
@@ -11,12 +12,17 @@ const args = process.argv.splice(2, process.argv.length).join(' ');
 
 export function track(cb) {
   if (!!segmentKey) {
+    const command = `${cmd} ${args}`;
+    const argv = _.forEach(yargs.argv, (val) => !!val);
+    const versions = Object.assign(getVersions(), {
+      os: {
+        platform: os.platform(),
+        release: os.release()
+      }
+    });
+    const properties = { command, argv, versions };
+
     const analytics = new Analytics(segmentKey, { flushAt: 1, flushAfter: 1 });
-    const properties = {
-      command: `${cmd} ${args}`,
-      argv: _.forEach(yargs.argv, (val) => !!val),
-      versions: getVersions()
-    };
     analytics.track({ event: 'command', userId: getUserId(), properties }, cb);
   } else {
     cb();
