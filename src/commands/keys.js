88073ba1c19603ae655d690034c053d03b21ef13
Jeremy Shimko <jeremy.shimko@gmail.com>
2017-02-23 00:27:25 -0500

    build the rest of the deploy key management methods

--- src/commands/keys.js
+++ src/commands/keys.js
@@ -1,6 +1,5 @@
 import fs from 'fs';
-import uuid from 'uuid';
-import { GraphQL, Log, exists } from '../utils';
+import { Config, GraphQL, Log, exists } from '../utils';
 
 const helpMessage = `
 Usage:
@@ -10,9 +9,10 @@ Usage:
     Commands:
       add       Add an SSH public key for deploying to Launchdock
       list      Show your existing SSH keys
+      delete    Remove an existing SSH public key from Launchdock
 `;
 
-export function keys(yargs) {
+export async function keys(yargs) {
   Log.args(yargs.argv);
 
   const subCommands = yargs.argv._;
@@ -21,6 +21,7 @@ export function keys(yargs) {
     return Log.default(helpMessage);
   }
 
+  // add
   if (subCommands[1] === 'add') {
     if (!subCommands[2]) {
       return Log.error('Public key path required');
@@ -36,20 +37,133 @@ export function keys(yargs) {
     try {
       publicKey = fs.readFileSync(publicKeyPath, 'utf8');
     } catch (e) {
-      return Log.error('Error reading pucli key');
+      return Log.error('Error reading public key');
     }
 
     const gql = new GraphQL();
 
-    return gql.fetch(`
+    const res = await gql.fetch(`
       mutation createKey($id: ID! $publicKey: String! ) {
         createKey(id: $id, publicKey: $publicKey) {
           id
-          fingerprint
-          owner
-          uuid
         }
       }
-    `, { id: uuid.v1(), publicKey });
+    `, { id: publicKeyPath.replace(/^.*?([^\\\/]*)$/, '$1'), publicKey });
+
+    if (!!res.errors) {
+      res.errors.forEach((err) => {
+        Log.error(err.message);
+      });
+      process.exit(1);
+    }
+
+    const updatedKeys = await gql.fetch(`
+      query {
+        sshKeys {
+          id
+          publicKey
+      		fingerprint
+        }
+      }
+    `);
+
+    if (!!updatedKeys.errors) {
+      updatedKeys.errors.forEach((err) => {
+        Log.error(err.message);
+      });
+      process.exit(1);
+    }
+
+    if (updatedKeys.data.sshKeys.length === 0) {
+      Log.info('\nNo SSH keys found.\n');
+      Log.info(`Run ${Log.magenta('reaction keys add /path/to/key.pub')} to upload one.\n`);
+      return Config.unset('global', 'launchdock.keys');
+    }
+
+    Config.set('global', 'launchdock.keys', updatedKeys.data.sshKeys);
+  }
+
+
+  // list
+  if (subCommands[1] === 'list') {
+
+    const gql = new GraphQL();
+
+    const result = await gql.fetch(`
+      query {
+        sshKeys {
+          id
+          publicKey
+      		fingerprint
+        }
+      }
+    `);
+
+    if (!!result.errors) {
+      result.errors.forEach((err) => {
+        Log.error(err.message);
+      });
+      process.exit(1);
+    }
+
+    if (result.data.sshKeys.length === 0) {
+      Log.info('\nNo SSH keys found.\n');
+      Log.info(`Run ${Log.magenta('reaction keys add /path/to/key.pub')} to upload one.\n`);
+      return Config.unset('global', 'launchdock.keys');
+    }
+
+    result.data.sshKeys.forEach((k) => Log.info(k.id));
+
+    Config.set('global', 'launchdock.keys', result.data.sshKeys);
+  }
+
+
+  // delete
+  if (subCommands[1] === 'delete') {
+    if (!subCommands[2]) {
+      return Log.error('Public key name required');
+    }
+
+    const publicKeyId = subCommands[2];
+
+    const gql = new GraphQL();
+
+    const res = await gql.fetch(`
+      mutation deleteKey($id: ID!) {
+        deleteKey(id: $id) {
+          success
+        }
+      }
+    `, { id: publicKeyId });
+
+    if (!!res.errors) {
+      res.errors.forEach((err) => {
+        Log.error(err.message);
+      });
+      process.exit(1);
+    }
+
+    const updatedKeys = await gql.fetch(`
+      query {
+        sshKeys {
+          id
+          publicKey
+          fingerprint
+        }
+      }
+    `);
+
+    if (!!updatedKeys.errors) {
+      updatedKeys.errors.forEach((err) => {
+        Log.error(err.message);
+      });
+      process.exit(1);
+    }
+
+    if (updatedKeys.data.sshKeys.length === 0) {
+      return Config.unset('global', 'launchdock.keys');
+    }
+
+    Config.set('global', 'launchdock.keys', updatedKeys.data.sshKeys);
   }
 }
