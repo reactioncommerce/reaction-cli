8f28667204679cccaefeb2a52d3c57479d2b9180
Jeremy Shimko <jeremy.shimko@gmail.com>
2017-03-03 01:12:03 -0500

    oops.  missing file in last commit

---
+++ src/utils/registry.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+
+export function setRegistryEnv(file) {
+  let registryData;
+
+  try {
+    registryData = fs.readFileSync(file, 'utf8');
+  } catch (error) {
+    Log.error(`Error reading Reaction Registry file: ${Log.magenta(file)}`);
+    process.exit(1);
+  }
+
+  // console.log(registryData);
+
+  process.env.REACTION_REGISTRY = registryData;
+
+  return JSON.parse(registryData);
+}
