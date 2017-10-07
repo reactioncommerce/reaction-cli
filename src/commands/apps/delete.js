f75a67979022e67aababd22b3d4c5a15f3a16aaf
Jeremy Shimko <jeremy.shimko@gmail.com>
2017-10-07 19:14:50 -0400

    add confirmation prompt before deleting an app

--- src/commands/apps/delete.js
+++ src/commands/apps/delete.js
@@ -1,8 +1,9 @@
 import _ from 'lodash';
 import { exec } from 'shelljs';
+import inquirer from 'inquirer';
 import { Config, GraphQL, Log } from '../../utils';
 
-export default async function appDelete({ name }) {
+export default function appDelete({ name }) {
   const apps = Config.get('global', 'launchdock.apps', []);
   const app = _.filter(apps, (a) => a.name === name)[0];
 
@@ -13,24 +14,34 @@ export default async function appDelete({ name }) {
 
   exec(`git remote remove ${app.group.namespace}-${name}`, { silent: true });
 
-  const gql = new GraphQL();
+  inquirer.prompt([{
+    type: 'confirm',
+    name: 'delete',
+    message: '\nAre you sure? There\'s no going back!',
+    default: false
+  }]).then(async (answers) => {
+    if (answers.delete) {
+      const gql = new GraphQL();
 
-  const res = await gql.fetch(`
-    mutation appDelete($_id: ID!) {
-      appDelete(_id: $_id) {
-        success
+      const res = await gql.fetch(`
+        mutation appDelete($_id: ID!) {
+          appDelete(_id: $_id) {
+            success
+          }
+        }
+      `, { _id: app._id });
+
+      if (!!res.errors) {
+        res.errors.forEach((err) => {
+          Log.error(`Failure deleting app deployment: ${err.message}`);
+        });
+        process.exit(1);
       }
+
+      Config.set('global', 'launchdock.apps', _.reject(apps, (a) => a.name === name));
+
+      Log.success(`\nApp deployment '${name}' successfully deleted.\n`);
     }
-  `, { _id: app._id });
+  });
 
-  if (!!res.errors) {
-    res.errors.forEach((err) => {
-      Log.error(`Failure deleting app deployment: ${err.message}`);
-    });
-    process.exit(1);
-  }
-
-  Config.set('global', 'launchdock.apps', _.reject(apps, (a) => a.name === name));
-
-  Log.success(`\nApp deployment '${name}' successfully deleted.\n`);
 }
