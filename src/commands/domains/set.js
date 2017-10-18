775f111f6e9c2f5e53eac7743381f31ce6827ba0
Jeremy Shimko <jeremy.shimko@gmail.com>
2017-10-18 14:50:05 -0400

    do a DNS lookup for new domains to ensure they’re pointed at our ingress

--- src/commands/domains/set.js
+++ src/commands/domains/set.js
@@ -1,7 +1,10 @@
+import dns from 'dns';
 import _ from 'lodash';
+import Promise from 'bluebird';
 import appsList from '../apps/list';
 import { Config, GraphQL, Log } from '../../utils';
 
+const resolveCname = Promise.promisify(dns.resolveCname);
 
 export default async function domainSet({ name, domain }) {
 
@@ -12,30 +15,43 @@ export default async function domainSet({ name, domain }) {
     return Log.error('\nApp deployment not found');
   }
 
-  const gql = new GraphQL();
+  const ingress = Config.get('cli', 'launchdock.ingress');
 
-  const result = await gql.fetch(`
-    mutation domainSet($appId: ID! $domain: String!) {
-      domainSet(appId: $appId, domain: $domain) {
-        name
-        domain
-      }
-    }
-  `, { appId: app._id, domain });
-
-  if (!!result.errors) {
-    result.errors.forEach((err) => {
-      Log.error(err.message);
-    });
+  let cNames;
+  try {
+    cNames = await resolveCname(domain);
+  } catch(err) {
+    Log.error('Failed to check your domain. Please contact support.');
     process.exit(1);
   }
 
-  const ingress = Config.get('cli', 'launchdock.ingress');
+  if (cNames.includes(ingress)) {
+    const gql = new GraphQL();
 
-  Log.info(`\nAdded new domain ${Log.magenta(domain)} to app ${Log.magenta(name)}\n`);
-  Log.info(`Make sure your domain name is pointed at:  ${Log.magenta(ingress)}\n`);
+    const result = await gql.fetch(`
+      mutation domainSet($appId: ID! $domain: String!) {
+        domainSet(appId: $appId, domain: $domain) {
+          name
+          domain
+        }
+      }
+    `, { appId: app._id, domain });
 
-  await appsList();
+    if (!!result.errors) {
+      result.errors.forEach((err) => {
+        Log.error(err.message);
+      });
+      process.exit(1);
+    }
 
-  return result.data.domainSet;
+    Log.info(`\nAdded new domain ${Log.magenta(domain)} to app ${Log.magenta(name)}\n`);
+
+    await appsList();
+
+    return result.data.domainSet;
+  }
+
+  Log.error(`\nERROR: It looks like '${domain}' is not yet pointed at '${ingress}'.`);
+  Log.error('This is required for app routing and automatic SSL certificates. Please try again once your DNS is updated.\n');
+  process.exit(1);
 }
