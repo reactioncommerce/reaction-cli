53f5974bcabc53fb96dabc094f6c1073f9f1dc09
MichaelJenny <michael.jenny@projekt-und-partner.de>
2017-12-19 15:24:17 +0100

    Extend build step `Asset provisioning`:
    1) Additionally copies public assets into the root applications /public folder
       Results in something along the lines:
       cp -R /imports/plugins/custom/<my-plugin-directory-name>/public/* /public/plugins/<plugin-name-from-registry-js>/
    
    2) Uses plugin name from registry.js to determine target-directory to
    copy into. This allows to choose the directory name of <my-plugin-directory-name>
    freely without having to change paths (e.g. URLs, Asset API, Urls-in-CSS-files) within the plugin.

--- src/utils/asset-provisioner.js
+++ src/utils/asset-provisioner.js
@@ -1,24 +1,69 @@
 import fs  from 'fs-extra'
 import path from 'path';
+import { default as vm } from 'vm';
 import Log from './logger';
 import { exists, getDirectories } from './fs';
 
-
-
-
 /**
  * getAssetPaths: Get pathnames of all asset directories from each plugin.
- * @param {String} baseDirPath - path to a plugins sub-directory (core/included/custom)
- * @return {Array} - Array of objects that contains plugin name & absolutepath of plugins `/private` directory
+ * @param {String} appRoot - absolute path to project root
+ * @param {String} pluginRoot - path to a plugins sub-directory (core/included/custom)
+ * @return {Array} - Array of objects that contain absolute source & destination paths for copy process
  */
-function getAssetPaths(baseDirPath) {
+function getAssetPaths(appRoot, pluginRoot) {
   const assetPaths = [];
   // get all plugin directories at provided base path
-  const pluginDirs = getDirectories(baseDirPath);
-  pluginDirs.forEach((plugin) => {
-    const assetDirectory = baseDirPath + plugin + '/private';
-    if (exists(assetDirectory)) {
-      assetPaths.push({name: plugin, dir: assetDirectory});
+  const pluginDirs = getDirectories(pluginRoot);
+  pluginDirs.forEach((pluginDir) => {
+    const pluginPath = pluginRoot + pluginDir;
+    const registryImport = pluginPath + '/register.js';
+    let pluginName = ""; // this is where the result of register.js execution goes into.
+    if (exists(registryImport)) {
+      try {
+        const content = fs.readFileSync(registryImport, "utf8");
+        if (content  && content.length) {
+          // Strip out all import statements.
+          const code = content.replace(/import.*\n/g, "");
+          const sandbox = {
+            // Mock Reaction.registerPackage function
+            Reaction: {
+              registerPackage(config) {
+                pluginName = config.name;
+              }
+            },
+            _: {
+              // lodash's extend is still used in a couple of register.js
+              extend() {
+                return Object.assign(...arguments);
+              }
+            }
+          };
+          vm.createContext(sandbox);
+          vm.runInContext(code, sandbox);
+        }
+      } catch (err) {
+        Log.error(`Can't execute file ${registryImport}: ${err}`);
+        process.exit(1);
+      }
+
+      if (pluginName) {
+        const privateResourcesDirectory = pluginPath + '/private';
+        if (exists(privateResourcesDirectory)) {
+          assetPaths.push({
+            source: privateResourcesDirectory,
+            destination: path.join(appRoot, "private/plugins", pluginName)
+          });
+        }
+        const publicResourcesDirectory = pluginPath + '/public';
+        if (exists(publicResourcesDirectory)) {
+          assetPaths.push({
+            source: publicResourcesDirectory,
+            destination: path.join(appRoot, "public/plugins", pluginName)
+          });
+        }
+      } else {
+        Log.info(`Can't determine plugin name for plugin directory ${pluginPath}. No resources copied.`);
+      }
     }
   });
   return assetPaths;
@@ -26,14 +71,12 @@ function getAssetPaths(baseDirPath) {
 
 /**
  * copyAssets: Copy all asset files into application's /private folder
- * @param {String} appRoot - application root path
- * @param {Array} assetDirs - Array of objects that contains plugin name & absolutepath of plugins `/private` directory
+ * @param {Array} assetDirs - Array of objects that contain the absolute source and destination paths for copying the assets.
  * @return undefined
  */
-function copyAssets(appRoot, assetDirs){
-  for (const {dir, name} of assetDirs) {
-    const targetDir = path.join(appRoot, "private/plugins", name);
-    fs.copySync(dir, targetDir);
+function copyAssets(assetDirs){
+  for (const {source, destination} of assetDirs) {
+    fs.copySync(source, destination);
   }
 }
 
@@ -46,10 +89,10 @@ export default function () {
   const includedPlugins = pluginsPath + 'included/';
   const customPlugins = pluginsPath + 'custom/';
 
-  const core = getAssetPaths(corePlugins);
-  const included = getAssetPaths(includedPlugins);
-  const custom = getAssetPaths(customPlugins);
+  const core = getAssetPaths(appRoot, corePlugins);
+  const included = getAssetPaths(appRoot, includedPlugins);
+  const custom = getAssetPaths(appRoot, customPlugins);
 
   const assetDirs = [].concat(core, included, custom);
-  copyAssets(appRoot, assetDirs);
+  copyAssets(assetDirs);
 }
