13da5c91ea8fc6522e63d8664a62d98385da8544
MichaelJenny <michael.jenny@projekt-und-partner.de>
2017-12-20 09:14:56 +0100

    PR change requests - Linting - Cleanup Step - Error handling for sync API calls

--- src/utils/asset-provisioner.js
+++ src/utils/asset-provisioner.js
@@ -1,5 +1,6 @@
-import fs  from 'fs-extra'
+import fs from 'fs-extra';
 import path from 'path';
+import rimraf from 'rimraf';
 import { default as vm } from 'vm';
 import Log from './logger';
 import { exists, getDirectories } from './fs';
@@ -7,23 +8,24 @@ import { exists, getDirectories } from './fs';
 /**
  * getAssetPaths: Get pathnames of all asset directories from each plugin.
  * @param {String} appRoot - absolute path to project root
- * @param {String} pluginRoot - path to a plugins sub-directory (core/included/custom)
+ * @param {String} subDir - plugin sub-directory (core/included/custom)
  * @return {Array} - Array of objects that contain absolute source & destination paths for copy process
  */
-function getAssetPaths(appRoot, pluginRoot) {
+function getAssetPaths(appRoot, subDir) {
   const assetPaths = [];
+  const pluginRoot = path.join(appRoot, '/imports/plugins/', subDir);
   // get all plugin directories at provided base path
   const pluginDirs = getDirectories(pluginRoot);
   pluginDirs.forEach((pluginDir) => {
     const pluginPath = pluginRoot + pluginDir;
     const registryImport = pluginPath + '/register.js';
-    let pluginName = ""; // this is where the result of register.js execution goes into.
+    let pluginName = ''; // this is where the result of register.js execution goes into.
     if (exists(registryImport)) {
       try {
-        const content = fs.readFileSync(registryImport, "utf8");
+        const content = fs.readFileSync(registryImport, 'utf8');
         if (content  && content.length) {
           // Strip out all import statements.
-          const code = content.replace(/import.*\n/g, "");
+          const code = content.replace(/import.*\n/g, '');
           const sandbox = {
             // Mock Reaction.registerPackage function
             Reaction: {
@@ -41,8 +43,8 @@ function getAssetPaths(appRoot, pluginRoot) {
           vm.createContext(sandbox);
           vm.runInContext(code, sandbox);
         }
-      } catch (err) {
-        Log.error(`Can't execute file ${registryImport}: ${err}`);
+      } catch (error) {
+        Log.error(`Can't execute file ${registryImport}: ${error}`);
         process.exit(1);
       }
 
@@ -51,14 +53,14 @@ function getAssetPaths(appRoot, pluginRoot) {
         if (exists(privateResourcesDirectory)) {
           assetPaths.push({
             source: privateResourcesDirectory,
-            destination: path.join(appRoot, "private/plugins", pluginName)
+            destination: path.join(appRoot, 'private/plugins', pluginName)
           });
         }
         const publicResourcesDirectory = pluginPath + '/public';
         if (exists(publicResourcesDirectory)) {
           assetPaths.push({
             source: publicResourcesDirectory,
-            destination: path.join(appRoot, "public/plugins", pluginName)
+            destination: path.join(appRoot, 'public/plugins', pluginName)
           });
         }
       } else {
@@ -71,27 +73,34 @@ function getAssetPaths(appRoot, pluginRoot) {
 
 /**
  * copyAssets: Copy all asset files into application's /private folder
+ *
  * @param {Array} assetDirs - Array of objects that contain the absolute source and destination paths for copying the assets.
- * @return undefined
+ * @return {undefined}
  */
-function copyAssets(assetDirs){
-  for (const {source, destination} of assetDirs) {
-    fs.copySync(source, destination);
+function copyAssets(assetDirs) {
+  for (const { source, destination } of assetDirs) {
+    try {
+      fs.copySync(source, destination);
+    } catch (error) {
+      Log.error(`Can't copy files from ${source} to ${destination}: ${error}`);
+    }
   }
+}
+
+function cleanup(appRoot) {
+  rimraf(`${appRoot}/public/plugins/*`);
+  rimraf(`${appRoot}/private/plugins/*`);
 }
 
 export default function () {
   Log.info('Provisioning assets...\n');
 
   const appRoot = path.resolve('.').split('.meteor')[0];
-  const pluginsPath = path.join(appRoot, '/imports/plugins/');
-  const corePlugins = pluginsPath + 'core/';
-  const includedPlugins = pluginsPath + 'included/';
-  const customPlugins = pluginsPath + 'custom/';
+  cleanup(appRoot);
 
-  const core = getAssetPaths(appRoot, corePlugins);
-  const included = getAssetPaths(appRoot, includedPlugins);
-  const custom = getAssetPaths(appRoot, customPlugins);
+  const core = getAssetPaths(appRoot, 'core');
+  const included = getAssetPaths(appRoot, 'included');
+  const custom = getAssetPaths(appRoot, 'custom');
 
   const assetDirs = [].concat(core, included, custom);
   copyAssets(assetDirs);
